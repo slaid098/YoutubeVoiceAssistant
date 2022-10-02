010367f2b3f2830c15573451c937dc16753d91d6
S <alon26518@gmail.com>
2022-10-02 04:45:15 +0300

    restart driver, every time get url

--- browser.py
+++ browser.py
@@ -8,6 +8,46 @@ from selenium.webdriver import Chrome
 from loguru import logger
 
 
+def get_url(url: str = "") -> None:
+    try:
+        _close_driver()
+        Driver.chrome = set_chrome_driver()
+        _browser_to_front()
+        _get_url(url)
+    except WebDriverException as ex:
+        logger.warning(f"{type(ex)} {ex}")
+    except Exception as ex:
+        logger.warning(f"{type(ex)} {ex}")
+
+
+def _close_driver() -> None:
+    try:
+        if Driver.chrome is not None:
+            Driver.chrome.close()
+            Driver.chrome.quit()
+    except Exception:
+        pass
+
+
+def _browser_to_front() -> None:
+    try:
+        driver = Driver.chrome
+        if driver is not None:
+            driver.minimize_window()
+            driver.maximize_window()
+    except WebDriverException as ex:
+        logger.warning(f"{type(ex)} {ex}")
+    except Exception as ex:
+        logger.warning(f"{type(ex)} {ex}")
+
+
+def _get_url(url: str) -> None:
+    driver = Driver.chrome
+    if driver is not None:
+        if url != "":
+            driver.get(url)
+
+
 def set_chrome_driver() -> Chrome:
     path_webdriver = Path("driver")
 
@@ -23,17 +63,8 @@ def set_chrome_driver() -> Chrome:
         ChromeDriverManager(path=path_webdriver).install()), options=options)
 
 
-def get_url(url: str = "") -> None:
-    try:
-        global driver
-        driver.minimize_window()
-        driver.maximize_window()
-        if url != "":
-            driver.get(url)
-    except WebDriverException as ex:
-        logger.warning(f"{type(ex)} {ex}")
-    except Exception as ex:
-        logger.warning(f"{type(ex)} {ex}")
-
-
-driver = set_chrome_driver()
+class Driver:
+    """
+    stores an instance of the current browser
+    """
+    chrome: Chrome | None = None
